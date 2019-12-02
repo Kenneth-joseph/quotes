60272b0742b1d7ead80a265fdac7ad43a5dbabde
Kenneth-joseph <josephochieng812@gmail.com>
2019-12-02 13:02:18 +0300

    added my vote section

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {QuoteC} from '../quote-c';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-vote',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote.component.css']
 })
 export class VoteComponent implements OnInit {
+
+  upvote = 0;
+  downvote = 0;
+
+  like(){
+    this.upvote ++;
+  }
+
+  dislike(){
+    this.downvote ++;
+  }
 
   constructor() { }
 
